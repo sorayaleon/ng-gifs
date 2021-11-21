53be40e85518e50f37403955bd4f3010583171af
Soraya <soraya.l.g.88@gmail.com>
2021-11-21 19:17:00 +0100

    Get references to HTML objects. @ViewChild

--- src/app/components/gifs/search/search.component.ts
+++ src/app/components/gifs/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  // ! = Not null assertion operator
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  search() {
+    const value = this.txtSearch.nativeElement.value;
+    console.log(value);
+    this.txtSearch.nativeElement.value = '';
   }
 
 }
